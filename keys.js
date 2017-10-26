7296701a7a0c779ab71b94d890d5ff36a51792c6
Josh Pierce <jepierce1992@gmail.com>
2017-10-26 00:29:32 -0400

    I think working

---
+++ keys.js
@@ -0,0 +1,20 @@
+var twitterKeys = {
+  consumer_key: 'kiIeEBbw9S59jGJ7XHUV1hZL1',
+  consumer_secret: 'KPIFa0POmrzRShfCA9QGRJyJ9Qz6eGyRO4bgv9Bcv4GuVgN7b8',
+  access_token_key: '918209208469872645-8FZUx8PMN3kKH8aQGEDbEcmlKG3b98V',
+  access_token_secret: 'eFw9GOZgMqaOZN3jhfxhQXhwm8TXCG0XaMAg8HOAOhbhe',
+}
+
+var spotifyKeys = {
+  client_ID: '41aae3f7d73944809aef1ca3f18e47ea',
+  client_secret: 'f7469ff74f3c4557898b27bb943fd3de'
+}
+
+var omdbKeys = {
+  omdb_key: '40e9cece'
+}
+
+module.exports = {
+  twitterKeys,
+  spotifyKeys,
+  omdbKeys};
---
+++ liri.js
@@ -0,0 +1,179 @@
+
+
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var Request = require("request");
+var keys = require("./keys.js");
+var fs = require("fs");
+
+
+var userRequest = process.argv[2];
+var userInput = "";
+for (var i = 3; i < process.argv.length; i++) {
+  userInput = userInput + " " + process.argv[i];
+}
+
+//default settings for movies and songs
+var userInputMovie = "";
+for (var i = 3; i < process.argv.length; i++) {
+  userInputMovie = userInputMovie + process.argv[i] + "+";
+}
+
+
+var song = "";
+if (process.argv.length < 4 && process.argv[2] !== "do-what-it-says") {
+  song = "The Sign Ace of Base";
+}
+else {
+  song = userInput;
+}
+
+var movie = "";
+if (process.argv.length < 4 & process.argv[2] !== "do-what-it-says") {
+  movie = "mr+nobody";
+}
+else {
+  movie = userInputMovie;
+}
+
+var omdbURL = "http://www.omdbapi.com/?apikey=" + keys.omdbKeys.omdb_key + "&t=" + movie;
+
+switch (userRequest) {
+
+  case "my-tweets":
+    myTweets();
+    break;
+
+  case "spotify-this-song":
+    spotifyThis();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doTheThing();
+    break;
+}
+
+
+function myTweets() {
+
+  var twitter = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+  });
+
+  var params = {screen_name: "GAFollowers"};
+
+  twitter.get("statuses/user_timeline", params, function (error, tweets, response) {
+    if (error) {
+      console.log(error);
+    }
+    if (!error) {
+      for (var i = 0; i < tweets.length && i <= 20; i++){
+        console.log("***************************************************");
+        console.log(tweets[i].text);
+        console.log("Tweet created: " + tweets[i].created_at);
+      }
+    }
+  })
+}
+
+//spotify-this-song function
+function spotifyThis() {
+
+  var spotify = new Spotify({
+    id: keys.spotifyKeys.client_ID,
+    secret: keys.spotifyKeys.client_secret
+  });
+
+  spotify.search({type: "track", query: song}, function (err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+    console.log("Title: " + data.tracks.items[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    console.log("Listen here: " + data.tracks.items[0].external_urls.spotify);
+  });
+}
+
+//movie-this function
+function movieThis() {
+
+  Request(omdbURL, function (error, response, body) {
+    if (error){
+      return console.log("error: " + error);
+    }
+
+    console.log(JSON.parse(body));
+    var rottenTomatoes = "NA";
+
+    for (var i = 0; i < JSON.parse(body).Ratings.length; i++){
+      if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes"){
+        rottenTomatoes = JSON.parse(body).Ratings[i].Value;
+      }
+    }
+
+    console.log("***************************************************");
+    console.log("Movie Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + rottenTomatoes);
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("***************************************************");
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("***************************************************");
+    console.log("Actors: " + JSON.parse(body).Actors);
+  });
+}
+
+//do-what-it-says function
+function doTheThing() {
+  fs.readFile("random.txt","utf-8",function(error, data){
+
+    if (error) {
+      return console.log(error);
+    }
+
+    //splits the data received into an array
+    var dataArr = data.split(",");
+    var commandArr = [];
+    var queryArr = [];
+    //sorts random text commands out of random.txt
+    for (var i = 0; i < dataArr.length; i){
+      commandArr.push(dataArr[i]);
+      i += 2;
+    }
+
+    //sorts random text queries out of random.txt
+    for (var i = 1; i < dataArr.length; i){
+      queryArr.push(dataArr[i]);
+      i += 2;
+    }
+
+    //uses a random number to select a command and its corresponding query
+    var randomNumber = Math.round(Math.random() * (commandArr.length));
+
+    var randomCommand = commandArr[randomNumber];
+    console.log(randomCommand);
+
+    var randomQuery = queryArr[randomNumber];
+    console.log(randomQuery);
+
+    if (randomCommand === "spotify-this-song"){
+      song = randomQuery;
+      spotifyThis();
+    }
+    else if (randomCommand === "movie-this"){
+      movie = randomQuery;
+      omdbURL = "http://www.omdbapi.com/?apikey=" + keys.omdbKeys.omdb_key + "&t=" + movie;
+      movieThis();
+    }
+  })
+}
